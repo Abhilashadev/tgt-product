43ee3db507805e2edd8c38f29ea760934ff1f865
deab <deab@skidata.com>
2019-11-21 12:27:10 +0530

    Set up retail application

---
+++ src/main/java/com/target/retail/tgtproduct/TgtProductApplication.java
@@ -0,0 +1,13 @@
+package com.target.retail.tgtproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgtProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgtProductApplication.class, args);
+	}
+
+}
